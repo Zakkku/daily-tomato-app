b91c064f5577202358b2030e34b8e5f297d0d28a
Zach <wakuwakuzakku@gmail.com>
2023-10-29 21:08:54 +0900

    adjusted styles when day is clicked

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import './App.css';
 /* 
   1. Select day, turn it bold (default will be Monday)
@@ -6,21 +6,32 @@ import './App.css';
   3. Increment / decrement should work based on which day is selected
 */
 
-const days = ["M", "T", "W", "Th", "F", "Sat", "Sun"]
+const days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 
 export default function App() {
+
+  const [selectedDay, setSelectedDay] = useState("Mon");
+
+  const handleDayClick = (day) => {
+    setSelectedDay(day);
+  }
+
   return (
     <div className="App">
       <div className="window">
         <h2>🍅 Daily Tomato App 🍅</h2>
         {days.map((day) => (
-          <div key={day} className="tomato-box">
-            <h3>{day}</h3>
+          <div 
+          key={day} 
+          className={`tomato-box ${selectedDay === day ? 'selected' : ''}`}
+          onClick={() => handleDayClick(day)}
+          >
+            <h3 >{day}</h3>
             <div className="tomato-day-box"></div>
           </div>
         ))}
         <div className="buttons-container">
-          <div className="button">-</div>
+          <div  className="button">-</div>
           <div className="button">+</div>
         </div>
       </div>
